712b93e6fe32fbf73ece0ac25c7470f793d3430c
Ed Morales <emoralesb05@gmail.com>
2017-05-28 05:32:35 +0000

    98eaf28 fix(theme): remove duplicate functions that are already part of material (#633)

--- layout/layout.module.js
+++ layout/layout.module.js
@@ -6,7 +6,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdSidenavModule, MdToolbarModule, MdButtonModule, MdIconModule, MdCardModule, MdListModule } from '@angular/material';
+import { MdSidenavModule, MdToolbarModule, MdButtonModule, MdIconModule, MdCardModule, MdListModule, ScrollDispatchModule } from '@angular/material';
 import { TdLayoutComponent } from './layout.component';
 import { TdLayoutNavComponent } from './layout-nav/layout-nav.component';
 import { TdLayoutNavListComponent } from './layout-nav-list/layout-nav-list.component';
@@ -34,6 +34,7 @@ CovalentLayoutModule = __decorate([
     NgModule({
         imports: [
             CommonModule,
+            ScrollDispatchModule,
             MdSidenavModule,
             MdToolbarModule,
             MdButtonModule,
