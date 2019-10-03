3964eb3e0b5c475f8fdb2d20f5257d7f1c502297
Richa Vyas <richavyas02@gmail.com>
2019-10-03 23:56:00 +0000

    d6d44aa feat(schematics) : promote ng add to core (#1498)

---
+++ schematics/ng-add/components.js
@@ -0,0 +1,99 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var DynamicForms = /** @class */ (function () {
+    function DynamicForms() {
+    }
+    DynamicForms.prototype.enabled = function (options) {
+        return options.dynamicForms;
+    };
+    DynamicForms.prototype.dependency = function () {
+        return '@covalent/dynamic-forms';
+    };
+    return DynamicForms;
+}());
+exports.DynamicForms = DynamicForms;
+var Http = /** @class */ (function () {
+    function Http() {
+    }
+    Http.prototype.enabled = function (options) {
+        return options.http;
+    };
+    Http.prototype.dependency = function () {
+        return '@covalent/http';
+    };
+    return Http;
+}());
+exports.Http = Http;
+var Highlight = /** @class */ (function () {
+    function Highlight() {
+    }
+    Highlight.prototype.enabled = function (options) {
+        return options.highlight;
+    };
+    Highlight.prototype.dependency = function () {
+        return '@covalent/highlight';
+    };
+    return Highlight;
+}());
+exports.Highlight = Highlight;
+var Markdown = /** @class */ (function () {
+    function Markdown() {
+    }
+    Markdown.prototype.enabled = function (options) {
+        return options.markdown;
+    };
+    Markdown.prototype.dependency = function () {
+        return '@covalent/markdown';
+    };
+    return Markdown;
+}());
+exports.Markdown = Markdown;
+var FlavoredMarkdown = /** @class */ (function () {
+    function FlavoredMarkdown() {
+    }
+    FlavoredMarkdown.prototype.enabled = function (options) {
+        return options.flavoredMarkdown;
+    };
+    FlavoredMarkdown.prototype.dependency = function () {
+        return '@covalent/flavored-markdown';
+    };
+    return FlavoredMarkdown;
+}());
+exports.FlavoredMarkdown = FlavoredMarkdown;
+var Echarts = /** @class */ (function () {
+    function Echarts() {
+    }
+    Echarts.prototype.enabled = function (options) {
+        return options.echarts;
+    };
+    Echarts.prototype.dependency = function () {
+        return '@covalent/echarts';
+    };
+    return Echarts;
+}());
+exports.Echarts = Echarts;
+var TextEditor = /** @class */ (function () {
+    function TextEditor() {
+    }
+    TextEditor.prototype.enabled = function (options) {
+        return options.textEditor;
+    };
+    TextEditor.prototype.dependency = function () {
+        return '@covalent/text-editor';
+    };
+    return TextEditor;
+}());
+exports.TextEditor = TextEditor;
+var CodeEditor = /** @class */ (function () {
+    function CodeEditor() {
+    }
+    CodeEditor.prototype.enabled = function (options) {
+        return options.codeEditor;
+    };
+    CodeEditor.prototype.dependency = function () {
+        return '@covalent/code-editor';
+    };
+    return CodeEditor;
+}());
+exports.CodeEditor = CodeEditor;
+//# sourceMappingURL=components.js.map
---
+++ schematics/ng-add/version-names.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.covalentCoreVersion = '2.1.0';
+exports.materialVersion = '8.1.1';
+//# sourceMappingURL=version-names.js.map
