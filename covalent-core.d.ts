1b75831fa622c4203ed64dbb5baa04855679cd25
Christian Memije <christianmemije@users.noreply.github.com>
2020-02-06 00:26:31 +0000

    f0dd4c6 *BREAKING CHANGE* build(): remove primary entry points (#1610)

--- covalent-core.d.ts
+++ covalent-core.d.ts
@@ -2,5 +2,3 @@
  * Generated bundle index. Do not edit.
  */
 export * from './index';
-export { IconService as ɵb } from './common/services/icon.service';
-export { RouterPathService as ɵa } from './common/services/router-path.service';
